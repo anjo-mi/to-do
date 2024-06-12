a03e024a3a138afe2342e255ad86aecbb4d4e184
kyle <nojaim.k@gmail.com>
2024-06-12 17:51:18 -0400

    timer display functional max one item, runs 2 timers on same display

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.getElementById('add').addEventListener('click', addItem)
 document.getElementById('clear').addEventListener('click', clearDone)
 
 let tasks = 0
-
+console.log(Date.now())
 let list = document.getElementById('list')
 
 function startTimer(date){
@@ -13,12 +13,14 @@ function startTimer(date){
         let hours = Math.floor(time % (1000*60*60*24) / (1000*60*60))
         let minutes = Math.floor(time % (1000*60*60) / (1000*60))
         let seconds = Math.floor(time % (1000*60) / 1000)
+        document.querySelector('.timer').innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`
     },1000)
 
 }
 
 function addItem(){
     let addedItem = '  ' + document.getElementById('task').value
+    let doneBy = +new Date(document.querySelector('input[type = "datetime-local"]').value)
     if (addedItem !== '  '){
         tasks++
         let li = document.createElement('li')
@@ -34,12 +36,18 @@ function addItem(){
         if(tasks > 0) {
             document.querySelector('.didIt').style.display = 'block'
         }
+        if(doneBy){
+            startTimer(doneBy)
+            let timer = document.createElement('span')
+            timer.setAttribute('class', 'timer')
+            li.appendChild(timer)
+        }
     }
     document.getElementById('task').value = ''
 }
 
 function clearDone(){
-    let checks = document.querySelectorAll('input[type = checkbox]')
+    let checks = document.querySelectorAll('input[type = "checkbox"]')
     checks.forEach(item => {
         if (item.checked === true){
             item.parentElement.remove()
