3e70ae6d4eee5393cbebf8efe16882288e7ae60a
kyle <nojaim.k@gmail.com>
2024-06-12 15:29:22 -0400

    clear box appears after li added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 document.getElementById('add').addEventListener('click', addItem)
 
+let tasks = 0
 
 let list = document.getElementById('list')
 
 function addItem(){
     let addedItem = document.getElementById('task').value
     if (addedItem){
+        tasks++
         let li = document.createElement('li')
         let input = document.createElement('input')
         let chore = document.createElement('span')
@@ -16,6 +18,11 @@ function addItem(){
         li.appendChild(input)
         li.appendChild(chore)
         list.appendChild(li)
+        if(tasks > 0) {
+            document.querySelector('.didIt').style.display = 'block'
+        }
     }
     document.getElementById('task').value = ''
 }
+
+// decrement tasks foreach qsALL on checkboxes
