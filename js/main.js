5ae0a58dc4c5ce37e15086e51e9ae05c77f6dd88
kyle <nojaim.k@gmail.com>
2024-06-12 15:22:05 -0400

    function to add li's

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+document.getElementById('add').addEventListener('click', addItem)
 
+
+let list = document.getElementById('list')
+
+function addItem(){
+    let addedItem = document.getElementById('task').value
+    if (addedItem){
+        let li = document.createElement('li')
+        let input = document.createElement('input')
+        let chore = document.createElement('span')
+        li.setAttribute('class', 'listItem')
+        input.setAttribute('type', 'checkbox')
+        chore.setAttribute('class', 'chore')
+        chore.innerText = addedItem
+        li.appendChild(input)
+        li.appendChild(chore)
+        list.appendChild(li)
+    }
+    document.getElementById('task').value = ''
+}
